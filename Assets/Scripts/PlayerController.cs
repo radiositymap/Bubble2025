80a708b92dde08708c4e0a1fba95dec244450f81
radiositymap <radiositymap@gmail.com>
2025-01-25 18:16:16 +0800

    refined player motion

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,10 +20,26 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float xMotion = Input.GetAxis("Horizontal");
-        float yMotion = Input.GetAxis("Vertical");
         rbd.AddForce(new Vector2(xMotion * speed, 0));
-        weapon.RotateAround(weapon.position,
-            new Vector3(0, 0, 1), yMotion * rotateSpeed);
+        if (xMotion < 0 && transform.localEulerAngles.y < 90f)
+            transform.localEulerAngles = new Vector3(0, 180f, 0);
+        if (xMotion > 0 && transform.localEulerAngles.y > 90f)
+            transform.localEulerAngles = Vector3.zero;
+
+        if (Input.GetKeyDown(KeyCode.S)) {
+            // TODO swap sprite
+            // reduce collider
+            if (transform.localScale.y > 0.6f)
+                transform.localScale = new Vector3(1f, 0.5f, 1f);
+        }
+        if (Input.GetKeyUp(KeyCode.S)) {
+            if (transform.localScale.y < 0.6f)
+                transform.localScale = Vector3.one;
+        }
+
+        float yMotion = Input.GetAxis("Vertical");
+        weapon.RotateAround(transform.position,
+            new Vector3(0, 0, -1), yMotion * rotateSpeed);
 
         if (Input.GetKeyDown(KeyCode.Space)) {
             if (gun != null) {
